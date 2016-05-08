737ad227bc8e14fc07f965fd46a249f5f15b3cc3
MauriDV <mdellevedove12@gmail.com>
2016-05-08 01:23:37 -0300

    get and set for the pointsCards attribute and create function to counts points of cards

--- models/player.js
+++ models/player.js
@@ -8,22 +8,40 @@ function Player(name,card1,card2,card3){
 	this.card1=card1;
 	this.card2=card2;
 	this.card3=card3;
+	this.pointsCards=0; //Puntos de cartas.
 };
 
 Player.prototype.showCards = function() {
 	return (this.card1.show() +" "+this.card2.show() +" "+ this.card3.show());
 };
 
+Player.prototype.getPointsCards = function() {
+	return this.pointsCards;
+};
+
+Player.prototype.setPointsCards = function() {
+	this.pointsCards = Math.max(countPoints(this.card1,this.card2),countPoints(this.card2,this.card3),countPoints(this.card1,this.card3));
+};
+
+countPoints = function (c1,c2){
+	if(c1.compareSuit(c2)){
+		if (c1.number>=11 && c2.number>=11){
+			return 20;
+		}else if(c1.number>=11 && c2.number<11){
+			return (10+c2.number)+10;
+		}else if(c1.number<11 && c2.number>=11){
+			return (10+c1.number)+10;
+		}else if(c1.number<11 && c2.number<11){
+			return (c1.number+c2.number)+20;
+		}
+	}else{
+		return Math.max(c1.number,c2.number);
+	}
+}
+
 module.exports.player = Player;
 
-carta1 = new Card("1","espada");
-carta2 = new Card("12","oro");
-carta3 = new Card("4","basto")
-p = new Player("Mauri",carta1,carta2,carta3);
 
-console.log(p.showCards());
 
-d = new Deck();
-cartas = d.mix();
-console.log(cartas[1]);
+
 
