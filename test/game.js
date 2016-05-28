cc37d7ee4cfd11d1850a17f53afb7e928ecdb740
MauriDV <mdellevedove12@gmail.com>
2016-05-28 00:07:50 -0300

    add test for envido

--- test/game.js
+++ test/game.js
@@ -79,5 +79,64 @@ describe('Game', function() {
     });
 
   });
+  
+  describe("#Envido", function(){
+    var c1 = new Card(7, 'copa');
+    var c2 = new Card(3, 'copa');
+    var c3 = new Card(2, 'basto');
+
+    var ca1 = new Card(7, 'copa');
+    var ca2 = new Card(2, 'copa');
+    var ca3 = new Card(2, 'basto');
+
+    describe("Envido querido",function(){ 
+      it("should increment 2 points to jugador1",function(){
+        var jugador1 = new Player("jugador1",c1,c2,c3);
+        jugador1.setPointsCards();
+        var jugador2 = new Player("jugador2",ca1,ca2,ca3);
+        jugador2.setPointsCards();
+        jugador2.aceptar = true;
+        jugador1.aceptar = false;
+        jugador1.envido(jugador2,2);
+        expect(jugador1.pointsGame).to.be.eq(2);
+      });
+    });
+    describe("Envido no querido",function(){ 
+      it("should increment 1 points to jugador1",function(){
+        var jugador1 = new Player("jugador1",c1,c2,c3);
+        jugador1.setPointsCards();
+        var jugador2 = new Player("jugador2",ca1,ca2,ca3);
+        jugador2.setPointsCards();
+        jugador2.aceptar = false;
+        jugador1.aceptar = false;
+        jugador1.envido(jugador2,2);
+        expect(jugador1.pointsGame).to.be.eq(1);
+      });
+    });
+    describe("Envido-envido querido",function(){ 
+      it("should increment 4 points to jugador1",function(){
+        var jugador1 = new Player("jugador1",c1,c2,c3);
+        jugador1.setPointsCards();
+        var jugador2 = new Player("jugador2",ca1,ca2,ca3);
+        jugador2.setPointsCards();
+        jugador2.aceptar = "envido";
+        jugador1.aceptar = true;
+        jugador1.envido(jugador2,2);
+        expect(jugador1.pointsGame).to.be.eq(4);
+      });
+    });
+    describe("Envido-envido no querido",function(){ 
+      it("should increment 2 points to jugador2",function(){
+        var jugador1 = new Player("jugador1",c1,c2,c3);
+        jugador1.setPointsCards();
+        var jugador2 = new Player("jugador2",ca1,ca2,ca3);
+        jugador2.setPointsCards();
+        jugador2.aceptar = "envido";
+        jugador1.aceptar = false;
+        jugador1.envido(jugador2,2);
+        expect(jugador2.pointsGame).to.be.eq(2);
+      });
+    });
+  });
 
 });
