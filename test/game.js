7e02500a8fefcd3fa320a1e0c044a0b98d761350
pato <patoserra74@hotmail.com>
2016-06-20 13:59:49 -0300

    test game

--- test/game.js
+++ test/game.js
@@ -8,135 +8,112 @@ var Player = playerModel.player;
 var Card = cardModel.card;
 
 describe('Game', function() {
+  var game;
 
-  describe('#Deal Cards', function() {
+  beforeEach(function(){
+    game = new Game();
+    game.player1 = new Player({ name: 'mauricio',aux:0});
+    game.player2 = new Player({ name: 'patricio',aux:0});
+		game.currentHand=game.player1;    
+		game.newRound();
+    // Force to have the following cards and envidoPoints
+    game.player1.setPointsCards(
+        new Card(1, 'copa'),
+        new Card(7, 'oro'),
+        new Card(2, 'oro')
+    );
 
-    it('#Players have cards', function(){
-      jugador1 = new Player("P1");
-      jugador2 = new Player("P2");
-      var g = new Game(jugador1,jugador2);
-      g.dealCards(jugador1,jugador2);
-      expect(jugador1.card1).to.not.be.a('null');
-      expect(jugador1.card2).to.not.be.a('null');
-      expect(jugador1.card3).to.not.be.a('null');
-      expect(jugador2.card1).to.not.be.a('null');
-      expect(jugador2.card2).to.not.be.a('null');
-      expect(jugador2.card3).to.not.be.a('null');
-    });
-
+    game.player2.setPointsCards(
+        new Card(6, 'copa'),
+        new Card(7, 'copa'),
+        new Card(2, 'basto')
+    );
   });
 
-  describe("#Points of cards", function(){
+	it('should save a game', function(done){
+    var game = new Game({ currentHand: 'player1' });
+    player1 = new Player({ name: 'mauricio' });
+    player2 = new Player({ name: 'patricio' });
 
-    describe("when cards are 11-12 or 12-11", function(){
-      var c1 = new Card(12, 'copa');
-      var c2 = new Card(11, 'copa');
-      var c3 = new Card(2, 'basto');
-
-      it("should returns 20", function(){
-        jugador = new Player("jugador",c1,c2,c3);
-        jugador.setPointsCards();
-        expect(jugador.getPointsCards()).to.be.eq(20);
-      });
-      it("should returns 20", function(){
-        jugador = new Player("jugador",c2,c1,c3);
-        jugador.setPointsCards();
-        expect(jugador.getPointsCards()).to.be.eq(20);
-      });
-
+    player1.save(function(err, player1) {
+      if(err)
+        done(err)
+      game.player1 = player1;
+      player2.save(function(err, player2) {
+        if(err)
+          done(err)
+       game.player2 = player2;
+        game.save(function(err, model){
+          if(err)
+            done(err)
+          expect(model.player1.nickname).to.be.eq('J');
+          expect(model.player2.nickname).to.be.eq('X');
+          done();
+        });
+      })
     });
+  });	
 
-    describe("when cards are y-x or x-y where x<11 and y>=11", function(){
-      var c1 = new Card(12, 'copa');
-      var c2 = new Card(4, 'copa');
-      var c3 = new Card(2, 'basto');
+  it('plays [envido, quiero] should gives 2 points to winner', function(){
+    game.play(game.player1, 'envido');
+    game.play(game.player2, 'quiero');
 
-      it("should returns 24 (y-x)", function(){
-        jugador = new Player("jugador",c1,c2,c3);
-        jugador.setPointsCards();
-        expect(jugador.getPointsCards()).to.be.eq(24);
-      });
-
-      it("should returns 24 (x-y)", function(){
-        jugador = new Player("jugador",c2,c1,c3);
-        jugador.setPointsCards();
-        expect(jugador.getPointsCards()).to.be.eq(24);
-      });
-
-    });
-
-    describe("when cards are y-x (x<11 and y<11)", function(){
-      var c1 = new Card(7, 'copa');
-      var c2 = new Card(2, 'copa');
-      var c3 = new Card(2, 'basto');
-
-      it("should returns 29", function(){
-        jugador = new Player("jugador",c1,c2,c3);
-        jugador.setPointsCards();
-        expect(jugador.getPointsCards()).to.be.eq(29);
-      });
-
-    });
-
+		expect(game.score[0]).to.be.equals(0);
+		expect(game.score[1]).to.be.equals(2);
+    
   });
-  
-  describe("#Envido", function(){
-    var c1 = new Card(7, 'copa');
-    var c2 = new Card(3, 'copa');
-    var c3 = new Card(2, 'basto');
 
-    var ca1 = new Card(7, 'copa');
-    var ca2 = new Card(2, 'copa');
-    var ca3 = new Card(2, 'basto');
+	it('plays [envido, noQuiero] should gives 1 points to winner', function(){
+    game.play(game.player1, 'envido');
+    game.play(game.player2, 'noQuiero');
 
-    describe("Envido querido",function(){ 
-      it("should increment 2 points to jugador1",function(){
-        var jugador1 = new Player("jugador1",c1,c2,c3);
-        jugador1.setPointsCards();
-        var jugador2 = new Player("jugador2",ca1,ca2,ca3);
-        jugador2.setPointsCards();
-        jugador2.aceptar = true;
-        jugador1.aceptar = false;
-        jugador1.envido(jugador2,2);
-        expect(jugador1.pointsGame).to.be.eq(2);
-      });
-    });
-    describe("Envido no querido",function(){ 
-      it("should increment 1 points to jugador1",function(){
-        var jugador1 = new Player("jugador1",c1,c2,c3);
-        jugador1.setPointsCards();
-        var jugador2 = new Player("jugador2",ca1,ca2,ca3);
-        jugador2.setPointsCards();
-        jugador2.aceptar = false;
-        jugador1.aceptar = false;
-        jugador1.envido(jugador2,2);
-        expect(jugador1.pointsGame).to.be.eq(1);
-      });
-    });
-    describe("Envido-envido querido",function(){ 
-      it("should increment 4 points to jugador1",function(){
-        var jugador1 = new Player("jugador1",c1,c2,c3);
-        jugador1.setPointsCards();
-        var jugador2 = new Player("jugador2",ca1,ca2,ca3);
-        jugador2.setPointsCards();
-        jugador2.aceptar = "envido";
-        jugador1.aceptar = true;
-        jugador1.envido(jugador2,2);
-        expect(jugador1.pointsGame).to.be.eq(4);
-      });
-    });
-    describe("Envido-envido no querido",function(){ 
-      it("should increment 2 points to jugador2",function(){
-        var jugador1 = new Player("jugador1",c1,c2,c3);
-        jugador1.setPointsCards();
-        var jugador2 = new Player("jugador2",ca1,ca2,ca3);
-        jugador2.setPointsCards();
-        jugador2.aceptar = "envido";
-        jugador1.aceptar = false;
-        jugador1.envido(jugador2,2);
-        expect(jugador2.pointsGame).to.be.eq(2);
-      });
-    });
+		expect(game.score[0]).to.be.equals(1);
+		expect(game.score[1]).to.be.equals(0);
+    
   });
+
+	it('plays [envido, envidox2, quiero] should gives 4 points to winner', function(){
+    game.play(game.player1, 'envido');
+    game.play(game.player2, 'envidox2');
+		game.play(game.player1, 'quiero');
+
+		expect(game.score[0]).to.be.equals(0);
+		expect(game.score[1]).to.be.equals(4);
+    
+  });
+
+	it('plays [envido, envidox2, noQuiero] should gives 2 points to winner', function(){
+    game.play(game.player1, 'envido');
+    game.play(game.player2, 'envidox2');
+		game.play(game.player1, 'noQuiero');
+
+		expect(game.score[0]).to.be.equals(0);
+		expect(game.score[1]).to.be.equals(2);
+  });
+
+	it('plays [playCard, playCard, playCard,playCard,playCard,playCard] should gives 1 points to winner', function()	{    
+		game.play(game.player1,'playCard',game.player1.card1);
+    game.play(game.player2,'playCard',game.player2.card3);
+		game.play(game.player2,'playCard',game.player2.card1);
+		game.play(game.player1,'playCard',game.player1.card2);
+		game.play(game.player1,'playCard',game.player1.card3);
+		game.play(game.player2,'playCard',game.player2.card2);
+		expect(game.score[0]).to.be.equals(1);
+		expect(game.score[1]).to.be.equals(0);
+    
+  });
+
+	it('plays [playCard, playCard,playCard,truco,quiero,playCard] should gives 2 points to winner', function()	{    
+		game.play(game.player1,'playCard',game.player1.card1);
+    game.play(game.player2,'playCard',game.player2.card1);
+		game.play(game.player1,'playCard',game.player1.card2);
+		game.play(game.player2,'truco');
+		game.play(game.player1,'quiero');
+		game.play(game.player2,'playCard',game.player2.card3);
+		expect(game.score[0]).to.be.equals(2);
+		expect(game.score[1]).to.be.equals(0);
+    
+  });
+
 
 });
